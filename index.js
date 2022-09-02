0d24aa9a8879cb4effc0bbd39c31e3f4b8cce896
Neftali Cespedes <cesinvestmentholdings@gmail.com>
2022-09-02 16:55:27 -0400

    commit tree 63a5e3f3ada99ef3b3a48010d38ef8ea0745f2e2

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+
+// const changeText = document.addEventListener('DOMContentLoaded', (event) => {
+//    const change = document.querySelector('p').innerText = ("This is really cool!");
+//    document.querySelector('p').append(change)
+//    console.log(changeText)
+// })
+
+const changeText = document.addEventListener('DOMContentLoaded', (event) => {
+    document.getElementById('text').textContent = 'This is really cool!'
+})
